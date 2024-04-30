a41324dc9a296f5fb96e10ae2195a3caf43d0f37
Eyosiyas <deadlyhacker607@gmail.com>
2024-04-30 18:47:30 -0400

    lists

--- 0x00-shell_basics/push.sh
+++ 0x00-shell_basics/push.sh
@@ -3,8 +3,13 @@
 
 git add .
 
-echo "Commit: "
-read commit
+commit=$1
 
-git commit -m "$commit"
-git push
+if [ $# -eq 0 ]
+then
+	echo "Put A Commit: "
+else
+	git commit -m "$commit"
+	git push
+fi
+
